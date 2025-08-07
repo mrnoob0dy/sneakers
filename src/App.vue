fe13a6eb0b316ca1716e45652b365f00fa0d8489
mrnoob0dy <jayna5155@yandex.ru>
2025-08-07 11:52:12 +0300

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
-<script setup></script>
+<script setup>
+import Header from "./components/Header.vue";
+</script>
 
 <template>
-  <h1>Hello</h1>
+  <div class="wrapper">
+    <Header></Header>
+  </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 body {
-  h1 {
-    font-size: 20px;
-  }
+  font-family: sans-serif;
+  font-weight: 400;
+  background-color: #e7f6ff;
+}
+
+.wrapper {
+  margin: 85px auto;
+  max-width: 80%;
+  width: 100%;
+  box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.04);
+  background: #fff;
+  border-radius: 20px;
+  padding: 45px 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,92 @@
+<template>
+  <header class="header">
+    <div class="header__left">
+      <img src="/logo.png" alt="Logo" width="40" height="40" />
+      <div class="header__text">
+        <h2 class="header__title">Vue Sneakers</h2>
+        <p class="header__description">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+    <ul class="nav">
+      <li class="nav__item">
+        <img src="/cart.svg" alt="Cart" />
+        <span class="cart">1205 руб.</span>
+      </li>
+      <li class="nav__item">
+        <img src="/heart.svg" alt="Heart" />
+        <span>Закладки</span>
+      </li>
+      <li class="nav__item">
+        <img src="/profile.svg" alt="Profile" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 40px;
+  border-bottom: 1px solid #eaeaea;
+  padding: 0 60px 44px;
+  margin-bottom: 45px;
+}
+
+.header__left {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+
+.header__text {
+  display: flex;
+  flex-direction: column;
+}
+
+.header__title {
+  font-weight: 700;
+  font-size: 20px;
+  text-transform: uppercase;
+  color: #000;
+}
+
+.header__description {
+  font-size: 14px;
+  color: #9d9d9d;
+}
+
+.nav {
+  display: flex;
+  align-items: center;
+  gap: 32px;
+}
+
+.nav__item {
+  display: flex;
+  align-items: center;
+  gap: 9px;
+  cursor: pointer;
+
+  span {
+    font-weight: 400;
+    font-size: 14px;
+    color: #5c5c5c;
+    transition: color 0.3s ease;
+  }
+
+  .cart {
+    font-weight: 600;
+    font-size: 14px;
+    color: #5c5c5c;
+  }
+
+  &:hover {
+    span {
+      color: black;
+    }
+  }
+}
+</style>
